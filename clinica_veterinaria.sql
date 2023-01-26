b3da8493cb0ebac8bba1a6ad232789eee8c80319
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-01-25 21:41:24 -0300

    banco de dados

--- clinica_veterinaria.sql
+++ clinica_veterinaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25-Jan-2023 às 00:15
+-- Tempo de geração: 26-Jan-2023 às 01:40
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -46,10 +46,11 @@ INSERT INTO `consulta` (`idconsulta`, `data_consulta`, `horario_consulta`, `desc
 (19, '2023-01-11', '13:00:00', ' Verificação anual', 'Negado', 24, 43),
 (21, '2023-01-13', '16:00:00', ' Verificação anual apenas', 'Indeferido', 23, 44),
 (24, '2023-01-27', '14:00:00', 'Exame anual apenas', 'A Cancelar', 23, 44),
-(25, '2023-01-26', '17:00:00', 'Precisa castrar ela pois esta muito emocionada', 'A Cancelar', 27, 40),
+(25, '2023-01-26', '17:00:00', 'Precisa castrar ela pois esta muito emocionada', 'Cancelado', 27, 40),
 (26, '2023-01-31', '16:00:00', 'Parece estar com dor nas ásas', 'A Confirmar', 24, 45),
-(27, '2023-01-24', '18:00:00', 'necessita averiguar o sangue', 'Confirmado', 23, 45),
-(28, '2023-02-15', '16:00:00', 'Meu galego  aparentemente está com dores nas costas', 'A Confirmar', 24, 44);
+(27, '2023-01-24', '18:00:00', 'necessita averiguar o sangue', 'Concluido', 23, 45),
+(28, '2023-02-15', '16:00:00', 'Meu galego  aparentemente está com dores nas costas', 'A Confirmar', 24, 44),
+(29, '2023-03-16', '18:00:00', 'Apenas uma consulta de rotina', 'Confirmado', 21, 40);
 
 -- --------------------------------------------------------
 
@@ -104,16 +105,15 @@ CREATE TABLE `pet` (
 --
 
 INSERT INTO `pet` (`idpet`, `nome_pet`, `data_nascimento_pet`, `sexo`, `imagem_pet`, `imagem_carteira_pet`, `idraca`, `usuario`) VALUES
-(38, 'Sofia', '2012-01-01', 'Fêmea', '63cf1d31741ae.jpg', NULL, 24, 90),
+(38, 'Sofia', '2012-01-01', 'Fêmea', '63cf1d31741ae.jpg', '63d0a9a9cb1d6.jpg', 24, 90),
 (40, 'Luna', '2019-12-05', 'Fêmea', NULL, NULL, 5, 90),
 (42, 'Felinina', '0000-00-00', 'Fêmea', '63bc9992c9bb1.jpg', NULL, 24, 100),
 (43, 'Luna', '0000-00-00', 'Fêmea', NULL, NULL, 5, 100),
-(44, 'Galego', '2009-09-01', 'Macho', NULL, NULL, 17, 90),
+(44, 'Galego', '2009-09-01', 'Macho', '63d175ccceeda.jpg', NULL, 17, 90),
 (45, 'Picapal', '0000-00-00', 'Macho', NULL, NULL, 33, 98),
 (46, 'Luna Herculano', '2016-01-01', 'Fêmea', NULL, NULL, 14, 104),
 (47, 'Luna 2', '2003-11-29', 'Fêmea', NULL, NULL, 15, 104),
-(48, 'Esparta', '2015-04-25', 'Macho', NULL, NULL, 11, 104),
-(49, 'Fernandinho Sem Vergonha', '2007-01-01', 'Macho', NULL, NULL, 26, 105);
+(48, 'Esparta', '2015-04-25', 'Macho', NULL, NULL, 11, 104);
 
 -- --------------------------------------------------------
 
@@ -187,7 +187,8 @@ INSERT INTO `tipo_consulta` (`idtipo_consulta`, `nome_consulta`, `valor_consulta
 (21, 'Consulta Dentária', 150, 'Exame como dentista veterinário                   ', 'Ativo', '01:00:00', '63c9a565bc521.png'),
 (23, 'Exame De Sangue', 180, 'Exame sanguíneo do animal para verificar a saúde', 'Ativo', '01:00:00', '63acb6b131bcd.png'),
 (24, 'Consulta Médica', 200, 'Avaliação geral do animal por uma médico veterinário geral    ', 'Ativo', '02:00:00', '63acde444571f.png'),
-(27, 'Castração', 500, 'cirurgia de castração', 'Ativo', '02:00:00', '63b1e4393353d.png');
+(27, 'Castração', 500, 'cirurgia de castração', 'Ativo', '02:00:00', '63b1e4393353d.png'),
+(31, 'Teste1', 150, 'teste1', 'Inativo', '02:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -217,11 +218,12 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`idusuario`, `nome_usuario`, `cpf`, `rg`, `data_nascimento`, `email`, `tipo_usuario`, `sit_usuario`, `endereco`, `foto_usuario`, `senha_usuario`, `chave`, `recuperar_senha`) VALUES
 (1, 'maria', '123', '123', '2022-09-28', 'maria@clinica.com', 'mantenedor', 'Ativo', 62, '63be21ec74b84.jpg', '$2y$10$Q6pkE2iA9fepKpF6EJqJeufYRIpjSQYxW5pmycYhfGiAbC7FCOQtS', NULL, NULL),
-(90, 'João Pedro Sgobero', '07894971406', '28289054898', '2000-11-29', 'joao@clinica.com', 'cliente', 'Ativo', 80, NULL, '$2y$10$ItnrAU4pPIpXa2/SEs3eSeOGihdrXJVhDLpWbusLgmPkRH55KtFcW', '$2y$10$AeyKCfce0ndyuFJk23eKZuaYWcbvLK6J5tU02DqOGbt1NVoa6GREC', NULL),
-(98, 'Marcos Barbosa Vieira', '32311285523', '12344537', '1963-07-25', 'marcos@marcos.com', 'cliente', 'Ativo', 81, NULL, '$2y$10$CxdAn1iPbrZ1YbTDGQHJjuUEDes/QmH9J7YcZYPkNJy1SPpfte.1S', '$2y$10$C1M1jEZA6U.sxfdKaaRS.eXJcrUMZvJiCKNQsoE5zvYJJhcow.m6.', NULL),
-(100, 'Matheus Laurentino', '07894971405', '6749209475', '2003-11-29', 'matheus@clinica.com', 'cliente', 'Ativo', 79, '63bc9984cab36.jpg', '$2y$10$qPn34zTeSm3jlurOPONB8OVbBYNDCEvvoJHTqnYcQq0knQU7QNGr6', '$2y$10$EIc4yWU2zyZRNpGH4nYs1O2B7pezcFjR.2Pez0BROqISmm5hYFfdC', NULL),
-(104, 'Nicolas Herculano', '07894971423', '12345564543', '2001-02-02', 'nicolas@clinica.com', 'cliente', 'Ativo', 82, NULL, '$2y$10$sUC4H/Iq1ejtbZMQiDvytubR33xtpPIiUxp/POou/iAPt2.JgvIji', '$2y$10$uuQHND3HMpb8xNFuNJ/nkeLkcsxprUv0JZf946R606p0sE4K7weQS', NULL),
-(105, 'Lara', '07894971409', '123543256563', '2001-01-01', 'lara@clinica.com', 'cliente', 'Ativo', 86, NULL, '$2y$10$eCrxozdP5agKlfLwkr9GEOEODJ4IMzgC0DSyvjilhHBdOTkdHgn.K', '$2y$10$T5JkwiGIb6AxjnxkmUP7Z.ef9IC.DMFcRSXrVG9CYpuLdLl6XRRsC', '$2y$10$llbVVNZ7MtZ9CNiL04KqqOuwxPIlcAI1417.iZAoohgqe46wR3h9.');
+(90, 'João Pedro Sgobero', '07894971406', '28289054898', '2000-11-29', 'joao@clinica.com', 'cliente', 'Ativo', 80, '63d06f18b09c3.jpg', '$2y$10$plYrgyWpu8TS.EEIQud7ZO4dP6/1m2/nnmr/9sGvRtrgcbAIhpKX6', NULL, '$2y$10$bk8puB4NlYTf86Y7uOuCj.dD11y4C3JAEeza08/dihsbt8Akqo2yy'),
+(98, 'Marcos Barbosa Vieira', '32311285523', '12344537', '1963-07-25', 'marcos@marcos.com', 'cliente', 'Ativo', 81, NULL, '$2y$10$CxdAn1iPbrZ1YbTDGQHJjuUEDes/QmH9J7YcZYPkNJy1SPpfte.1S', NULL, NULL),
+(100, 'Matheus Laurentino', '07894971405', '6749209475', '2003-11-29', 'matheus@clinica.com', 'cliente', 'Ativo', 79, '63bc9984cab36.jpg', '$2y$10$qPn34zTeSm3jlurOPONB8OVbBYNDCEvvoJHTqnYcQq0knQU7QNGr6', NULL, NULL),
+(104, 'Nicolas Herculano', '07894971423', '12345564543', '2001-02-02', 'nicolas@clinica.com', 'cliente', 'Ativo', 82, NULL, '$2y$10$sUC4H/Iq1ejtbZMQiDvytubR33xtpPIiUxp/POou/iAPt2.JgvIji', NULL, NULL),
+(106, 'Iluska Maria', '07894971480', '12323434566', '1970-11-06', 'iluska@clinica.com', 'cliente', 'Ativo', NULL, NULL, '$2y$10$h2dcNr6aHnTvamd0rQ8xZeKoe2gMhTV4E3vawMJHXgor9IvEJgOE2', NULL, NULL),
+(111, 'Thomas El Kapo', '1232', '1232', '2002-11-11', 't1@gmail.com', 'cliente', 'Confirmando', NULL, NULL, '$2y$10$IoFM45QuQWYy7L3UEf8aSOuzAgPcYHRSuDgT382dxuoSA7g6qBVyC', '$2y$10$5heNZkKl42D9q/cvLclxMOY..lV7WpGVurpk6TVjlEO/csjYbq8lG', NULL);
 
 --
 -- Índices para tabelas despejadas
@@ -276,7 +278,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `consulta`
 --
 ALTER TABLE `consulta`
-  MODIFY `idconsulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `idconsulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de tabela `endereco`
@@ -288,7 +290,7 @@ ALTER TABLE `endereco`
 -- AUTO_INCREMENT de tabela `pet`
 --
 ALTER TABLE `pet`
-  MODIFY `idpet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `idpet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT de tabela `raca_pet`
@@ -300,13 +302,13 @@ ALTER TABLE `raca_pet`
 -- AUTO_INCREMENT de tabela `tipo_consulta`
 --
 ALTER TABLE `tipo_consulta`
-  MODIFY `idtipo_consulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `idtipo_consulta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
 
 --
 -- Restrições para despejos de tabelas
